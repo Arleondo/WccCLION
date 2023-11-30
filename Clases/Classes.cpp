f559a21102eb062a885ce6761222e5059b2485a8
Arleondo <arnolsmith.tacca@ucsp.edu.pe>
2023-11-29 20:31:51 -0600

    Updete de codigo de fibonacci
    Aca el fibonacci tiene cpp xd

--- Clases/Classes.cpp
+++ Clases/Classes.cpp
@@ -6,9 +6,10 @@ int fibonacci(int n){
 
     return fibonacci(n-1)+fibonacci(n-2);
 }
-
+/*
 int main() {
     int x=2, y=5;
     cout<<"El fibonacci de la multiplicacion de "<<x<<" y "<<y<<" es: "<<fibonacci(x*y)<<endl;
     return 0;
 }
+*/
