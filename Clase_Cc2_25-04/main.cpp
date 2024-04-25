13079446c681e6f3fe165c97cefa43031f7f1e48
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2024-04-25 16:09:18 +0200

    class Advance
    From today

--- Clase_Cc2_25-04/main.cpp
+++ Clase_Cc2_25-04/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
+    int i, n;
+    char *buffer;
 
+    cout << "Que tan larga desea la string:" << endl ;
+    cin >> i;
 
+    buffer = (char*) malloc (i+1);
+    if (!buffer){exit (1);}
 
+    for (n=0; n<i; n++){
+        buffer[n]=rand()%26+'a';
+    }
+    buffer[i]='\0';
+    cout << "La string random :\n" << buffer << endl ;
+    free (buffer);
 }
